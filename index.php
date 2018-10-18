09218e5b0d92638a74950eea039d62450cda9835
DifPlanet <difplanet069@gmail.com>
2018-10-18 18:04:07 +0400

    Инозмецева, Талутова, Яйков

---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Резюме Кошки</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Кошачье резюме :3</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+                <a class="nav-link" href="index.php">Домой  <span class="sr-only">(current)</span><img src="files/icon.png" width="30" height="30"></a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="photos.php">Фотографии</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+<br>
+<div align="center">
+    <h2>Кошачье резюме</h2>
+    <p class="text-body">Конечно, вы можете подумать, что это глупо - делать
+        резюме для Кошки. Но это отнюдь не так, ведь Кошка - очень
+        трудоспособный, несмотря на то, что у него лапки.</p>
+    <table width="700">
+        <tr>
+            <td><h4>Кошка Александровна</h4></td>
+        </tr>
+        <tr>
+            <td><img src="files/main.jpg" width="350" height="250"></td>
+            <td>
+                <h5>Дата и место рождения</h5>
+                <p class="active">Родилась 5 мая 2000 года<br>
+                    Место рождения: город Кошачий, округ Рыбки
+                </p>
+                <h5>Образование</h5>
+                <p class="active">Закончила школу с золотой медалью в 2002 году,
+                    а в 2005 - Государственный Кошачий Университет им. Клеопатры
+                    с красным дипломом. В 2007 успешно завершила Высшие Курсы кошачьей режиссуры
+                    и сценаризма. Также имеются Сертификаты о повышении Кошачьей квалификации.
+                </p>
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2">
+                <h5>Сведения о предыдущих местах работы</h5>
+                <p class="active">
+                    <b>Октябрь 2007 - Сентябрь 2009</b><br>
+                    <i>Инспектор коробок.</i> Место работы: Управление Министерства Кошачьих Дел.
+                    Причина увольнения: Выдавали недостаточно корма.<br>
+                    <b>Май 2010 - Апрель 2011</b><br>
+                    <i>Будильник.</i> Место работы: Федеральная Служба Исполнения Наказаний.
+                    Причина увольнения: Повышение.<br>
+                    <b>Май 2011 - Август 2011</b><br>
+                    <i>Психотерапевт.</i> Место работы: Федеральная Служба Исполнения Наказаний.
+                    Причина увольнения: Нашлась более интересная работа.<br>
+                    <b>Ноябрь 2011 - Июль 2013</b><br>
+                    <i>Искатель приведений.</i> Место работы: Федеральная Миграционная Служба.
+                    Причина увольнения: Слишком опасная работа, захотелось поспокойнее (годы уже не те).<br>
+                    <b>Июль 2013 - Февраль 2017</b><br>
+                    <i>Нагреватель постельки.</i> Место работы: Налоговая Инспекция.
+                    Причина увольнения: Корпорация обанкротилась.<br>
+                </p>
+                <h5>Умения и навыки</h5>
+                <p class="active">Имеются навыки 20-ти часового сна, кидания предметов со стола,
+                    а также умение делать тыгыдык среди ночи.
+                </p>
+                <h5>Иное</h5>
+                <p class="active">Люблю рыбку и молочко. Сделаю ваш дом таким же пушистым, как я сама.
+                </p>
+            </td>
+        </tr>
+    </table>
+</div>
+
+<footer class="modal-footer">
+    <p class="disabled">&copy;Кошачьи права защищены, 2018</p>
+</footer>
+</body>
---
+++ photos.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Резюме Кошки</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="#">Кошачье резюме :3</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="index.php">Домой</a>
+            </li>
+            <li class="nav-item active">
+                <a class="nav-link" href="#">Фотографии  <span class="sr-only">(current)</span><img src="files/icon.png" width="30" height="30"></a>
+            </li>
+        </ul>
+    </div>
+</nav>
+<div align="center">
+    <h2>Фотографии с предыдущих мест работы</h2>
+    <p class="text-body">Ниже представлены фото, подтверждающие кошачью трудоспособность.</p>
+</div>
+<table align="center">
+    <tr>
+        <td><div class="card" style="width: 18rem;">
+            <img class="card-img-top" src="files/1.jpg" alt="Card image cap">
+            <div class="card-body">
+                <p class="card-text">Октябрь 2007 - Сентябрь 2009</p>
+            </div>
+        </div></td>
+        <td><div class="card" style="width: 18rem;">
+    <img class="card-img-top" src="files/3.jpg" alt="Card image cap">
+    <div class="card-body">
+        <p class="card-text">Май 2010 - Апрель 2011</p>
+    </div>
+        </div></td>
+        <td><div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="files/2.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text">Май 2011 - Август 2011</p>
+                </div>
+            </div></td>
+    </tr>
+    <tr>
+        <td><div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="files/4.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text">Ноябрь 2011 - Июль 2013</p>
+                </div>
+            </div></td>
+        <td><div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="files/5.jpg" alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-text">Июль 2013 - Февраль 2017</p>
+                </div>
+            </div></td>
+    </tr>
+</table>
+<footer class="modal-footer">
+    <p class="disabled">&copy;Кошачьи права защищены, 2018</p>
+</footer>
+</body>
